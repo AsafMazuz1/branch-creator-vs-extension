fd79bd0a89df7aec9b52c4ad33dfb7eae844e591
AsafMazuz1 <Asafmazuz@gmail.com>
2024-02-29 20:42:07 +0200

    Remove console.log and add one more default white list

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import { registerCreateBranchCommand, registerSwitchBranchCommand, registerValidateBranchCommand } from './gitCommands';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Your extension "branch-creator" is now active!');
-
     let createBranchDisposable = registerCreateBranchCommand();
     context.subscriptions.push(createBranchDisposable);
 
